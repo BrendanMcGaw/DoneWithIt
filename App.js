22c347e9156554996b1343ef238a9512dcbf1186
Brendan McGaw <Brendanmcgaw@gmail.com>
2023-11-05 00:10:46 +1100

    Added a new safe view for android,custom.

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider, useSafeAreaInsets } from "react-native-safe-area-context";
+import {Platform, SafeAreaView, View, StyleSheet, Text, Image} from 'react-native';
+
+function SafeScreen() {
+  const insets = useSafeAreaInsets();
+  return (
+    <View style={{ flex: 1, paddingTop:insets.top}}>
+      <Text style={{ fontSize: 28}}>Content is in a safe place.</Text>
+    </View>
+  )
+}
 
 export default function App() {
-  let x  = 1;
-  console.log("App Executed");
   
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Hello React Native,  we're  making some  of this textr super  longh and weird and long, so that we can test if athe app hates us for th e length of the thingswe did.</Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeScreen />
+      <View style={styles.container}>
+        <Text style={styles.fontStyle}>Hello React Native, we're making some of this text super long and weird and long, so that we can test if the app hates us for the length of the things we did.</Text>
+        <Image source={{
+          width: 200,
+          height: 300,
+          uri:"https://picsum.photos/200/300"}} />
+      </View>
+    </SafeAreaProvider>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'dodgerblue',
+    // alignItems: "center",
+    // justifyContent: "center",
   },
+  fontStyle: {
+    fontSize: 24,
+    PaddingTop: 36,
+  }
 });
